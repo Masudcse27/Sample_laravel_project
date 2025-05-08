2a130de7467ad17b38324992166d75480ff1be76
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2025-05-08 16:56:03 +0600

    Registration and Login with JWT

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::get('/test', [TestController::class, 'index']);
+
+Route::controller(AuthController::class)->group(function (){
+    Route::post('login','login')->name('login');
+    Route::post('register','register')->name('user.register');
+});
